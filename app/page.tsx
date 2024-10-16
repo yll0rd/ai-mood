8f6200d4b105f5ecc90d98a4e2c61102f930093a
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-10-16 21:02:27 +0100

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <div className="w-screeen h-screen bg-black flex justify-center items-center text-white">
+            <div className="w-full max-w-[600px] mx-auto space-y-4">
+                <h1 className="text-6xl">The best Journal app, period.</h1>
+                <p className="text-2xl text-white/60">Thia is the best app for tracking your mood through out your life. All you have to do is to be honest</p>
+                <div>
+                    <Link href={"/journal"}>
+                        <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">Get Started</button>
+                    </Link>
+
+                </div>
+            </div>
+        </div>
+    );
+}
