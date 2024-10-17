63685c90743c589d3553ce73b3cce3f445ea1741
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-10-17 07:49:39 +0100

    feat: set up globalForPrisma and prisma models

---
+++ utils/db.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client"
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
+}
+
+export const db = globalForPrisma.prisma ?? new PrismaClient({
+    log: ['query']
+})
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
