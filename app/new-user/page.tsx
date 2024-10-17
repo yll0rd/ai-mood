0efb0305792f2b927d0537eadc2bd9ab91fb62e9
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-10-17 08:53:24 +0100

    feat: implemented creation of new user

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,9 +1,36 @@
+import { db } from '@/utils/db';
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-const NewUser = () => {
+const createNewUser = async () => {
+    const user = await currentUser();
+    // console.log(user);
+    
+    const match = !!(await db.user.findFirst({
+        where: {
+            clerkId: user!.id
+        }
+    }))
+
+    if (!match) {
+        await db.user.create({
+            data: {
+                clerkId: user!.id,
+                email: user!.emailAddresses[0].emailAddress as string
+            }
+        })
+    }
+
+    redirect("/journal")
+
+}
+
+const NewUserPage = async () => {
+    await createNewUser()
     return (
-        <div>Heyy</div>
+        <div>...Loading</div>
     );
 };
 
-export default NewUser;
+export default NewUserPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
-import Image from "next/image";
+import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+
+    const  href = (auth().userId) ? "journal" : "/new-user"
     return (
         <div className="w-screeen h-screen bg-black flex justify-center items-center text-white">
             <div className="w-full max-w-[600px] mx-auto space-y-4">
                 <h1 className="text-6xl">The best Journal app, period.</h1>
                 <p className="text-2xl text-white/60">Thia is the best app for tracking your mood through out your life. All you have to do is to be honest</p>
                 <div>
-                    <Link href={"/journal"}>
+                    <Link href={href}>
                         <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">Get Started</button>
                     </Link>
 
