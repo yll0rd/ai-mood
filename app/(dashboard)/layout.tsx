394ede0a7c092749de7858c626e7ff9bf4d958b7
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-10-20 22:49:10 +0100

    added sidebar links

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,11 @@
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 import React from "react";
+
+const links = [
+	{ href: "/", label: "Home" },
+	{ href: "/journal", label: "Journal" },
+];
 
 const DashboardLayout = ({
 	children,
@@ -9,13 +15,20 @@ const DashboardLayout = ({
 	return (
 		<div className="h-screen w-screen relative">
 			<aside className="absolute top-0 w-[200px] left-0 border-r h-full border-black/10">
-				Mood
+				<div>Mood</div>
+				<ul>
+					{links.map(({ href, label }, id) => (
+						<li key={id} className="px-2 py-6 text-xl">
+							<Link href={href}>{label}</Link>
+						</li>
+					))}
+				</ul>
 			</aside>
 			<div className="ml-[200px]">
 				<header className="h-[60px] border-b border-black/10">
 					<div className="h-full w-full px-6 flex items-center justify-end">
-                        <UserButton />
-                    </div>
+						<UserButton />
+					</div>
 				</header>
 				<div className="h-[calc(100vh-60px)]">{children}</div>
 			</div>
